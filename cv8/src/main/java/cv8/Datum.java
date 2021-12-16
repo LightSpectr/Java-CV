d9c65e0c0d5594220e4cd23a28f9a842afc1314b
LightSpectr <maked440@gmail.com>
2021-12-16 16:46:41 +0100

    cv8 cv10

--- cv8/src/main/java/cv8/Datum.java
+++ cv8/src/main/java/cv8/Datum.java
@@ -10,5 +10,34 @@ package cv8;
  * @author makedo01
  */
 public class Datum {
+    private int den;
+    private int mesic;
+    private int rok;
     
+    private Datum(int den, int mesic, int rok) throws Exception {
+        if (den > 1 || den < 31){
+             this.den = den;
+        }
+        else{
+            throw new Exception("Wrong day");
+        }
+        if (mesic > 1 || mesic <12){
+            this.mesic = mesic;
+        }
+        else{
+            throw new Exception("Wrong month");
+        }
+        if (rok > 1 ){
+            this.rok = rok;
+        }
+        else{
+            throw new Exception("Wrong year");
+        }
+       
+    }
+    
+    public String toString() {
+      return Integer.toString(den)+'.'+Integer.toString(mesic)+'.'+
+              '.'+Integer.toString(rok);
+   }
 }
